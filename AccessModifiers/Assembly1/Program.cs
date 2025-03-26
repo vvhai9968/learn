8da6581a4e96b90d15a86a1d44de7172480d9b76
haivv <haivv@novaon.asia>
2025-03-26 13:29:51 +0700

    feat: access modifiers

---
+++ AccessModifiers/Assembly1/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Scope
+{
+    public class AssemblyBaseClass
+    {
+        private string PrivateVar = "private";
+        protected string ProtectedVar = "protected";
+        internal string InternalVar = "internal";
+        protected internal string ProtectedInternalVar = "protected internal";
+        public string PublicVar = "public";
+
+        public void TestAccess()
+        {
+            //Accessible
+            Console.WriteLine(PrivateVar);
+            Console.WriteLine(ProtectedVar);
+            Console.WriteLine(InternalVar);
+            Console.WriteLine(ProtectedInternalVar);
+            Console.WriteLine(PublicVar);
+        }
+        
+    }
+
+    public class AssemblyDerivedClass : AssemblyBaseClass
+    {
+        public void TestAccessDerivedClass()
+        {
+            //Not Accessible
+            //Console.WriteLine(PrivateVar);
+            
+            //Accessible
+            Console.WriteLine(ProtectedVar);
+            Console.WriteLine(InternalVar);
+            Console.WriteLine(ProtectedInternalVar);
+            Console.WriteLine(PublicVar);
+        }
+    }
+
+    public class AssemblyOtherClass
+    {
+        public void TestAccess()
+        {
+            var objBase = new AssemblyBaseClass();
+            //Not Accessible
+            // Console.WriteLine(objBase.PrivateVar);
+            // Console.WriteLine(objBase.ProtectedVar);
+            
+            //Accessible
+            Console.WriteLine(objBase.InternalVar);
+            Console.WriteLine(objBase.ProtectedInternalVar);
+            Console.WriteLine(objBase.PublicVar);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objBase = new AssemblyBaseClass();
+            objBase.TestAccess();
+            Console.WriteLine("----------------------------");
+            var objDerived = new AssemblyDerivedClass();
+            objDerived.TestAccessDerivedClass();
+            Console.WriteLine("----------------------------");
+            var objOtherBase = new AssemblyOtherClass();
+            objOtherBase.TestAccess();
+        }
+    }
+}
---
+++ AccessModifiers/Assembly2/Program.cs
@@ -0,0 +1,49 @@
+﻿using Scope;
+
+namespace Assembly2
+{
+    public class AssemblyDerivedClass : AssemblyBaseClass
+    {
+        public void TestAccessDerivedClass()
+        {
+            //Not Accessible
+            //Console.WriteLine(PrivateVar);
+            //Console.WriteLine(InternalVar);
+            
+            //Accessible
+            Console.WriteLine(ProtectedVar);
+            
+            Console.WriteLine(ProtectedInternalVar);
+            Console.WriteLine(PublicVar);
+        }
+    }
+    
+    public class AssemblyOtherClass
+    {
+        public void TestAccess()
+        {
+            var objBase = new AssemblyBaseClass();
+            //Not Accessible
+            // Console.WriteLine(objBase.PrivateVar);
+            // Console.WriteLine(objBase.ProtectedVar);
+            //Console.WriteLine(objBase.InternalVar);
+            //Console.WriteLine(objBase.ProtectedInternalVar);
+            
+            //Accessible
+            Console.WriteLine(objBase.PublicVar);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            var objDerived = new AssemblyDerivedClass();
+            objDerived.TestAccessDerivedClass();
+            objDerived.TestAccess();
+            Console.WriteLine("----------------------------");
+            var objOther = new AssemblyOtherClass();
+            objOther.TestAccess();
+        }
+    }
+}
