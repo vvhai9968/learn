043549a50c095e6025cfda4084bb61e50b12bcff
haivv <haivv@novaon.asia>
2025-03-27 17:36:46 +0700

    feat: ref out in

---
+++ Ref_Out_In/Ref_Out_In/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ref_Out_In
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            ChangeValueRef(ref a);
+            ChangeValueOut(out var b);
+            ChangeValueIn(a);
+            Console.WriteLine(a);
+        }
+
+        static bool ChangeValueRef(ref int x)
+        {
+            x = 20;
+            return true;
+        }
+        
+        static bool ChangeValueOut(out int x)
+        {
+            x = 20;
+            return true;
+        }
+        
+        static bool ChangeValueIn(in int x)
+        {
+            return true;
+        }
+    }
+}
