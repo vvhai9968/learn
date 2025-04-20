976393010a0cfeab7cbb854344170e1e595d2601
haivv <vuvanhai568@gmail.com>
2025-04-20 15:04:48 +0700

    Day_7 + Day_8

---
+++ LeetCode/Longest_Consecutive_Sequence_128/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Longest_Consecutive_Sequence_128;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        var res = LongestConsecutive([100, 14, 200, 10, 13, 12]);
+        Console.WriteLine(res);
+    }
+
+    private static int LongestConsecutive(int[] nums)
+    {
+        if (nums.Length == 0)
+            return 0;
+
+        var numSet = new HashSet<int>(nums);
+        var maxLength = 0;
+
+        foreach (var num in numSet)
+        {
+            if (numSet.Contains(num - 1)) continue;
+            var currentNum = num;
+            var currentStreak = 1;
+
+            while (numSet.Contains(currentNum + 1))
+            {
+                currentNum++;
+                currentStreak++;
+            }
+
+            if (currentStreak > maxLength)
+                maxLength = currentStreak;
+        }
+
+        return maxLength;
+    }
+}
---
+++ LeetCode/Valid_Palindrome_125/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Valid_Palindrome_125;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+        Console.WriteLine(IsPalindrome("race a car"));
+    }
+
+    public static bool IsPalindrome1(string s)
+    {
+        var newStr = "";
+        foreach (var c in s)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                newStr += char.ToLower(c);
+            }
+        }
+
+        var newStrReverse = new string(newStr.Reverse().ToArray());
+        return newStr == newStrReverse;
+    }
+
+    private static bool IsPalindrome(string s)
+    {
+        var filtered = new List<char>();
+
+        foreach (var c in s)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                filtered.Add(char.ToLower(c));
+            }
+        }
+
+        int left = 0, right = filtered.Count - 1;
+        while (left < right)
+        {
+            if (filtered[left] != filtered[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
